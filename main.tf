021caa6972da4ec3849ff9f774f55e4f983015ee
Luan Bui The <luanbt@tokyotechlab.com>
2024-12-07 18:59:03 +0700

    update

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ module "vpc" {
   single_nat_gateway   = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/demo-eks-cluster" = "shared"
   }
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/demo-eks-cluster" = "shared"
     "kubernetes.io/role/elb"               = 1
 
   }
   private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/demo-eks-cluster" = "shared"
     "kubernetes.io/role/private_elb"       = 1
 
   }
